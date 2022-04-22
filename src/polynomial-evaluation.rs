a44cd98860957601defcc01d54fbc9d13dbaa441
Naman Kumar <namankr02@gmail.com>
2022-04-22 16:54:31 +0530

    added `chi` in polynomial-evaluations

--- src/polynomial-evaluation.rs
+++ src/polynomial-evaluation.rs
@@ -1,3 +1,12 @@
-let initial_values = vec![]
-let initial_vector = vec![]
+let initial_values = vec![];
+let initial_vector = vec![];
+let prime;
+
+fn chi(w:vec<i32>, x:vec<i32>) -> i32 {
+    let mut counter = 1;
+    for i in 1..x.len() {
+        counter *= (x[i]*w[i]) + (1-x[i])*(1-w[i]);
+    }
+    return counter;
+}
 
