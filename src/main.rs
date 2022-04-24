b0c07ab69f866b40b1af1f5686277ddf452bb729
Naman Kumar <namankr02@gmail.com>
2022-04-24 16:44:36 +0530

    fixed incorrect padding error

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod polynomial_evaluation;
 
 fn main() {
-    let function: Vec<i32> = vec![1, 2, 3, 4];
-    let string = vec![3, 4];
+    let function: Vec<i32> = vec![4, 3, 1, 4];
+    let string = vec![1, 1];
     let prime = 7;
 
     println!("{}", polynomial_evaluation::f_tilde(function, string, prime));
--- src/polynomial_evaluation.rs
+++ src/polynomial_evaluation.rs
@@ -1,15 +1,16 @@
-fn chi(w:String, x:&Vec<i32>, p:i32) -> i32 {
+pub fn chi(w:String, x:&Vec<i32>, p:i32) -> i32 {
     let mut counter: i32 = 1;
     for i in 0..x.len() {
-        counter *= (x[i]*((w.as_bytes())[i] as i32)) + (1-x[i])*(1-((w.as_bytes())[i]) as i32);
+        counter *= (x[i]*((w.as_bytes())[i] as i32 - 0x30)) + (1-x[i])*(1-(((w.as_bytes())[i]) as i32 -0x30));
+        counter = ((counter % p) + p) % p;
     }
-    return counter % p;
+    return ((counter % p) + p) % p;
 }
 
 pub fn f_tilde(fun_list:Vec<i32>, x:Vec<i32>, p:i32) -> i32 {
     let mut counter: i32 = 0;
     for i in 0..fun_list.len() {
-        let j = format!("{:0width$}", format!("{:b}", i), width = x.len());
+        let j = format!("{:0>width$}", format!("{:b}", i), width = x.len());
         counter += fun_list[i]*chi(j, &x, p);
     }
     return counter % p;
