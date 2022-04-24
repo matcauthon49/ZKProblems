f04e2bcb8e1fc7bee589cb1bf896c7e172a0d4b8
Naman Kumar <namankr02@gmail.com>
2022-04-24 10:40:48 +0530

    fixed parsing error

--- src/polynomial_evaluation.rs
+++ src/polynomial_evaluation.rs
@@ -8,8 +8,9 @@ fn chi(w:String, x:&Vec<i32>, p:i32) -> i32 {
 
 pub fn f_tilde(fun_list:Vec<i32>, x:Vec<i32>, p:i32) -> i32 {
     let mut counter: i32 = 0;
-    for i in 1..fun_list.len() {
-        counter += fun_list[i]*chi(format!("{:b}", i), &x, p);
+    for i in 0..fun_list.len() {
+        let j = format!("{:0width$}", format!("{:b}", i), width = x.len());
+        counter += fun_list[i]*chi(j, &x, p);
     }
     return counter % p;
 }
