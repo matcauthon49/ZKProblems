1605251814a8189f6f54461beb73249a9af59d49
Naman Kumar <namankr02@gmail.com>
2022-04-24 16:46:33 +0530

    removed  from

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,4 @@
 mod polynomial_evaluation;
 
 fn main() {
-    let function: Vec<i32> = vec![4, 3, 1, 4];
-    let string = vec![1, 1];
-    let prime = 7;
-
-    println!("{}", polynomial_evaluation::f_tilde(function, string, prime));
 }
--- src/polynomial_evaluation.rs
+++ src/polynomial_evaluation.rs
@@ -1,4 +1,4 @@
-pub fn chi(w:String, x:&Vec<i32>, p:i32) -> i32 {
+fn chi(w:String, x:&Vec<i32>, p:i32) -> i32 {
     let mut counter: i32 = 1;
     for i in 0..x.len() {
         counter *= (x[i]*((w.as_bytes())[i] as i32 - 0x30)) + (1-x[i])*(1-(((w.as_bytes())[i]) as i32 -0x30));
