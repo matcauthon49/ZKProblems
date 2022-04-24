716ae18b9ca1d4ddb85d73640cb66c41deb6db1d
Naman Kumar <namankr02@gmail.com>
2022-04-24 10:07:14 +0530

    fixed minor error

--- src/polynomial_evaluation.rs
+++ src/polynomial_evaluation.rs
@@ -1,6 +1,6 @@
 fn chi(w:String, x:&Vec<i32>, p:i32) -> i32 {
     let mut counter: i32 = 1;
-    for i in 1..x.len() {
+    for i in 0..x.len() {
         counter *= (x[i]*((w.as_bytes())[i] as i32)) + (1-x[i])*(1-((w.as_bytes())[i]) as i32);
     }
     return counter % p;
