64df3b605361c4e803d1f6af5fa4749309d93481
Naman Kumar <namankr02@gmail.com>
2022-05-03 12:14:39 +0530

    changed crate name to match rust standards

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 mod polynomial_evaluation;
 
 fn main() {
+    let function_def:Vec<i32> = vec!(1, 12, 3, 4, 5, 6, 9, 10);
+    let prime:i32 = 17;
+    let to_evaluate:Vec<i32> = vec!(1,1,1);
+
+    println!("{}", polynomial_evaluation::f_tilde(function_def, to_evaluate, prime));
 }
